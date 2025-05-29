d730730ad492f399a1974e9904bc8bfe64f01e5f
Paris Dev <paris.paris.baghdasaryan@gmail.com>
2025-05-29 16:36:23 +0400

    sftp conn

--- node.js
+++ node.js
@@ -3,7 +3,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Node server!\n");
+  res.end("Hello from Node server222222222222222!\n");
 });
 
 const PORT = 8080;
