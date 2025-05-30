df2bb2ca5e875c078043c8ef8699a122e31c17c9
Paris Dev <paris.paris.baghdasaryan@gmail.com>
2025-05-30 11:26:58 +0400

    node

--- node.js
+++ node.js
@@ -3,7 +3,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("esel aaaaaaaaaaaaaaaaad!\n");
+  res.end("esel aaaaaaaaasssssssssssssssssssssaaaaaaaaad!\n");
 });
 
 const PORT = 8080;
