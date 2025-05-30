1d9949ae644f62da5088821b39948359ccd2ad42
Paris Dev <paris.paris.baghdasaryan@gmail.com>
2025-05-30 22:30:33 +0400

    a

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-    "apps" : [{
-      "name"        : "node-app",
-      "script"      : "./node.js",
-      "watch"       : false,
-      env: {
-        NODE_ENV: process.env.SSH_KEY
-      }
-    }]
-  }
+  apps: [{
+    name   : "node-app",
+    script : "./node.js",
+    watch  : false,
+
+    env: {
+      NODE_ENV: "development",
+      SSH_KEY: process.env.SSH_KEY
+    },
+    env_production: {
+      NODE_ENV: "production",
+      SSH_KEY: process.env.SSH_KEY
+    }
+  }]
+};
