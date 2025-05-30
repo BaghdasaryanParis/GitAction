a3c6fe97a0dc6049026976f9b3d34f2ec1c8ffdb
Paris Dev <paris.paris.baghdasaryan@gmail.com>
2025-05-30 22:35:42 +0400

    a

--- node.js
+++ node.js
@@ -3,7 +3,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello this is my ssh_key !\n" + process.env.NODE_ENV);
+  res.end("Hello this is my ssh_key !\n" + process.env.SSH_KEY);
 });
 
 const PORT = 8080;
